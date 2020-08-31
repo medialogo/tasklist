456f8a9f7c3c4e449fc2ae6daecd6d1188250170
t.ikemi (4230) <ikemi@medialogo.com>
2020-09-01 04:02:48 +0900

    before validation

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -8,11 +8,29 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class FolderPage implements OnInit {
   public folder: string;
+  title = 'タスク登録';
+  tasks: { name: string}[] = [
+  ];
+  task: string;
+
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
+  ionViewWillEnter() {
+    if ('tasks' in localStorage) {
+      this.tasks = JSON.parse(localStorage.tasks);
+    }
+  }
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+  }
+
+  addTask() {
+    this.tasks.push({
+      name: this.task
+    });
+    localStorage.tasks = JSON.stringify(this.tasks);
+    this.task = '';
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBPjVeiuO5F-zDmc5W_1IKndsV99V9VWwA',
+    authDomain: 'calendar-odoriba0.firebaseapp.com',
+    databaseURL: 'https://calendar-odoriba0.firebaseio.com',
+    projectId: 'calendar-odoriba0',
+    storageBucket: 'calendar-odoriba0.appspot.com',
+    messagingSenderId: '65994542599',
+    appId: '1:65994542599:web:1a8f6d7773e66cd9bfa4a2',
+    measurementId: 'G-K9B971C2BX'
+  }
 };
 
 /*
